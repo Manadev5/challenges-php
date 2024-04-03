74ee0c34d688f91857ac9b8d6fd56cf2824c9f2f
Manassè MUKENGE <manakabamba@gmail.com>
2024-04-03 13:22:23 +0200

    challenge-7

---
+++ challenge-php-7/debug.php
@@ -0,0 +1,24 @@
+<?php
+
+$characters = [
+    "Negan" => [
+        "city" => "The Sanctuary",
+        "weapon" => "Lucille"
+    ],
+    "Daryl" => [
+        "city" => "Alexandria",
+        "weapon" => "crossbow"
+    ],
+    "Ezekiel" => [
+        "city" => "The Kingdom",
+        "weapon" => "Shiva"
+    ],
+    "Manassé" => [
+        "city" => "Herblay",
+        "weapon" => "axe"
+    ]
+];
+
+echo "<pre>";
+var_dump($characters);
+echo "</pre>";
